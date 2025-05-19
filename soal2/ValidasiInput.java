695ff7347c6945b9090b1769c63e36405463ca39
EkoRamadn <ekoramdani7777@gmail.com>
2025-05-19 08:39:34 +0700

    membuat Validasi input user

---
+++ soal2/ValidasiInput.java
@@ -0,0 +1,23 @@
+package soal2;
+
+// package soal1;
+import java.util.Scanner;
+
+public class ValidasiInput {
+    public double validasiDouble(Scanner input) {
+        while (!input.hasNextDouble()) {
+            System.out.println("Masukkan angka yang valid!");
+            input.next();
+        }
+        return input.nextDouble();
+    }
+
+    public int validasiInt(Scanner input) {
+        while (!input.hasNextInt()) {
+            System.out.println("Masukkan angka yang valid!");
+            input.next();
+        }
+        return input.nextInt();
+
+    }
+}
